e5c4106760941cb2c1cd8e5dfb25c4c008879857
Azharuddin Khan <iamazharuddin10@gmail.com>
2020-04-08 19:08:24 +0530

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,29 +1,75 @@
 package com.example.myapplication
-import android.content.Intent
-import android.net.Uri
+
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.webkit.PermissionRequest
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
+
+const val url = "https://5e8472abaaa290cd3beb0f19--determined-goldwasser-f059bf.netlify.com"
 
 class MainActivity : AppCompatActivity() {
+    private val CAMERA_REQUEST_CODE = 101
+    private val TAG = "PermissionDemo"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val permission = ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.CAMERA)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }else{
+            openWebView()
+        }
+    }
 
-        val url = "https://5e8472abaaa290cd3beb0f19--determined-goldwasser-f059bf.netlify.com/"
-//        val myWebView: WebView = findViewById(R.id.webview)
-//        myWebView.settings.javaScriptEnabled = true
-//        myWebView.loadUrl(url)
-            val btn = findViewById<Button>(R.id.btn)
-             btn.setOnClickListener {
-            Intent(Intent.ACTION_VIEW, Uri.parse(url)).apply {
-                startActivity(this)
+    private fun openWebView(){
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.webViewClient = WebViewClient()
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.webChromeClient = object : WebChromeClient() {
+            override fun onPermissionRequest(request: PermissionRequest) {
+                request.grant(request.resources)
             }
         }
-
+        myWebView.loadUrl(url)
     }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_REQUEST_CODE)
+    }
+
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG, "Permission has been denied by user")
+                } else {
+                    openWebView()
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if(webview.canGoBack()){
+            webview.goBack()
+        }else {
+            super.onBackPressed()
+        }
+    }
+
 }
+
